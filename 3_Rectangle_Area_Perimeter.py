5056f46ac683678554f6c67bbb00cb2deb9d6478
sreenriitcyber18-arch <sreenriitcyber18@gmail.com>
2026-09-08 00:49:57 -0700

    Add files via upload

---
+++ 3_Rectangle_Area_Perimeter.py
@@ -0,0 +1,6 @@
+length=int(input("Enter the length:"))
+breadth=int(input("Enter the breadth:"))
+area=length*breadth
+perimeter=2*(length+breadth)
+print("area=",area)
+print("perimeter=",perimeter)
