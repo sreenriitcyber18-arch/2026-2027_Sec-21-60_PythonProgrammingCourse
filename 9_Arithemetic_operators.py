d1f9d0fe35b2b501df1deec8b5bda3b6701f2361
sreenriitcyber18-arch <sreenriitcyber18@gmail.com>
2026-09-08 20:19:53 -0700

    arithmetic operators
    #Arithmetic operators

---
+++ 9_Arithemetic_operators.py
@@ -0,0 +1,12 @@
+# Arithmetic operations on two numbers
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+print("Addition =", a + b)
+print("Subtraction =", a - b)
+print("Multiplication =", a * b)
+print("Division =", a / b)
+print("Floor Division =", a // b)
+print("Modulus =", a % b)
+print("Power =", a ** b)
