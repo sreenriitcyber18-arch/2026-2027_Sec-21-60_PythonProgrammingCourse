0c6cf86a325eacff2b9732fd667d2e1b2c8a2089
sreenriitcyber18-arch <sreenriitcyber18@gmail.com>
2026-09-08 20:20:54 -0700

    Relational Operators
    #Relational Operators

---
+++ 10_Relational_Operators.py
@@ -0,0 +1,16 @@
+
+# Program to demonstrate relational operators
+
+a = 10
+b = 5
+
+print("a =", a)
+print("b =", b)
+
+print("a > b  :", a > b)
+print("a < b  :", a < b)
+print("a == b :", a == b)
+print("a != b :", a != b)
+print("a >= b :", a >= b)
+print("a <= b :", a <= b)
+
