10135ddbb6bb517d6cdeda7631cd339df106c047
sreenriitcyber18-arch <sreenriitcyber18@gmail.com>
2026-09-08 23:21:23 -0700

    Task-13
    #Membership Operators

---
+++ 13_Membership_Operators.py
@@ -0,0 +1,22 @@
+
+# Program to demonstrate Membership Operators
+# in and not in
+
+numbers = [10, 20, 30, 40, 50]
+
+print("List:", numbers)
+
+# Using 'in' operator
+print(30 in numbers)
+
+# Using 'not in' operator
+print(60 not in numbers)
+
+# More examples
+print(20 in numbers)
+print(70 not in numbers)
+
+
+
+
+
