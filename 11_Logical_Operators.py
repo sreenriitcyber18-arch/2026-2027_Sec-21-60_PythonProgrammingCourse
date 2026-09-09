fcd25c642542b165afd77bf92ab711852a718ac5
sreenriitcyber18-arch <sreenriitcyber18@gmail.com>
2026-09-08 22:59:37 -0700

    Logical Oprators
    #Logical operators

---
+++ 11_Logical_Operators.py
@@ -0,0 +1,19 @@
+
+# Program to demonstrate logical operators
+
+a = True
+b = False
+
+print("a =", a)
+print("b =", b)
+
+# AND operator
+print("a and b =", a and b)
+
+# OR operator
+print("a or b =", a or b)
+
+# NOT operator
+print("not a =", not a)
+print("not b =", not b)
+
