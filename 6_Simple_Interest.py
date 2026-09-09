43cb9e37f2208b1b641178ac017a1cbd7b70f109
sreenriitcyber18-arch <sreenriitcyber18@gmail.com>
2026-09-08 19:19:46 -0700

    Simple interest
    #Simple interest

---
+++ 6_Simple_Interest.py
@@ -0,0 +1,7 @@
+principle = float(input("Enter the principal value: "))
+rate = float(input("Enter the rate value: "))
+time = float(input("Enter the time: "))
+
+si = principle * rate * time / 100
+
+print("SI =", si)
