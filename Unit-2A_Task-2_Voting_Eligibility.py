1c301308b91271df193d63c886897f0145e18a59
sreenriitcyber18-arch <sreenriitcyber18@gmail.com>
2026-09-25 03:42:11 -0700

    Unit-2A_Task-2
    Voting_Eligibility

---
+++ Unit-2A_Task-2_Voting_Eligibility.py
@@ -0,0 +1,5 @@
+age_in_years = int(input("Enter your age in years: "))
+if age_in_years >= 18:
+   print(f"Age {age_in_years} years: Eligible to vote")
+else:
+   print(f"Age {age_in_years} years: Not eligible to vote")
